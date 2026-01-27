ed92ddc408978674389bd5abd23101645f9b8d20
Jakub Kopystianski <jakub.kopystianski@infor.com>
2026-01-27 19:55:59 +0100

    Added 258 Theology sentences in polish and english

---
+++ data/raw/Theology.pl
@@ -0,0 +1,258 @@
+Bóg jest wszechwiedzący i zna wszystko.
+Wiara jest darem Boga dla ludzkości.
+Biblia jest świętą księgą chrześcijaństwa.
+Modlitwa jest sposobem komunikacji z Bogiem.
+Jezus Chrystus uważany jest za Syna Bożego.
+Zbawienie jest możliwe poprzez wiarę i łaskę.
+Duch Święty prowadzi wierzących w codziennym życiu.
+Kościół to wspólnota wierzących.
+Chrzest symbolizuje oczyszczenie i nowe życie.
+Zmartwychwstanie Jezusa jest centralnym punktem wiary chrześcijańskiej.
+Trójca Święta składa się z Ojca, Syna i Ducha Świętego.
+Grzech oddziela ludzi od Boga.
+Skrucha prowadzi do przebaczenia.
+Dziesięć Przykazań to prawa moralne dane przez Boga.
+Łaska to niezasłużona przychylność Boga.
+Eucharystia upamiętnia Ostatnią Wieczerzę.
+Miłość jest największym przykazaniem w chrześcijaństwie.
+Biblia zawiera Stary i Nowy Testament.
+Prorocy przekazywali ludziom słowo Boże.
+Niebo jest obiecane tym, którzy wierzą i podążają za Bogiem.
+Piekło jest opisane jako miejsce oddzielenia od Boga.
+Ewangelia oznacza "dobrą nowinę".
+Miłosierdzie jest ważną cnotą w teologii.
+Kościół obchodzi wiele świąt w ciągu roku.
+Święci są czczeni za swoje święte życie.
+Stworzenie to akt Boga powołujący wszechświat do istnienia.
+Życie wieczne jest fundamentalną nadzieją chrześcijańską.
+Modlitwa Pańska została nauczona przez Jezusa.
+Jezus według Ewangelii czynił cuda.
+Wiara bez uczynków jest martwa.
+Duch Święty zstąpił w dniu Pięćdziesiątnicy.
+Spowiedź grzechów przynosi duchowe uzdrowienie.
+Miłość Boga jest bezwarunkowa.
+Psalmy to starożytne modlitwy i hymny.
+Chrześcijanie wierzą w powtórne przyjście Chrystusa.
+Biblia naucza, by miłować bliźniego jak siebie samego.
+Apostołowie głosili przesłanie Jezusa.
+Credo Nicejskie podsumowuje doktrynę chrześcijańską.
+Chrześcijaństwo uczy o znaczeniu pokory.
+Sakramenty to święte obrzędy w Kościele.
+Sprawiedliwość Boża jest doskonała i prawdziwa.
+Aniołowie są posłańcami Boga.
+Biblia była pisana przez wiele wieków.
+Stary Testament zawiera opowieść o stworzeniu świata.
+Mojżesz wyprowadził Izraelitów z Egiptu.
+Łaska przynosi duszy pokój.
+Kościół gromadzi się na nabożeństwie w każdą niedzielę.
+Przebaczenie jest niezbędne do pojednania.
+Jezus nauczał o Królestwie Bożym.
+Wiara daje nadzieję w trudnych chwilach.
+Krzyż jest symbolem ofiary i odkupienia.
+Chrześcijaństwo powstało w pierwszym wieku naszej ery.
+Duch Święty przynosi pociechę i siłę.
+Wierzący są powołani do służby innym.
+Kościół naucza o życiu po śmierci.
+Bóg jest zarówno sprawiedliwy, jak i miłosierny.
+Nauczanie chrześcijańskie zachęca do miłosierdzia.
+Niebo jest opisane jako miejsce radości i pokoju.
+Do sakramentów należą chrzest i komunia.
+Bóg powołuje ludzi do życia w świętości.
+Biblia opisuje wiele przymierzy z Bogiem.
+Nowy Testament opowiada o życiu Jezusa.
+Kościół jest miejscem wspólnoty i kultu.
+Kazanie na Górze zawiera nauki Jezusa.
+Wiara może przenosić góry.
+Biblia jest tłumaczona na wiele języków.
+Zmartwychwstanie Jezusa daje nadzieję na życie wieczne.
+Kościół używa symboli, takich jak krzyż i ryba.
+Księga Objawienia mówi o końcu czasów.
+Uczniostwo oznacza naśladowanie przykładu Jezusa.
+Biblia mówi o miłości, sprawiedliwości i miłosierdziu.
+Studia teologiczne pomagają głębiej zrozumieć wiarę.
+Chrześcijaństwo rozprzestrzeniło się w Cesarstwie Rzymskim.
+Kościół wspiera ubogich i potrzebujących.
+Post jest duchową praktyką w chrześcijaństwie.
+Biblia ostrzega przed pychą i chciwością.
+Duch Święty obdarza wierzących darami duchowymi.
+Kościół obchodzi Wielkanoc i Boże Narodzenie.
+Obietnice Boże są godne zaufania.
+Biblia zachęca do modlitwy i dziękczynienia.
+Jedność chrześcijańska jest ważną wartością.
+Trójca Święta jest tajemnicą wiary.
+Kościół naucza o grzechu i odkupieniu.
+Maryja jest czczona jako Matka Jezusa.
+Chrześcijaństwo ma wiele wyznań.
+Kościół często modli się o pokój na świecie.
+Jezus nauczał w przypowieściach.
+Największym przykazaniem jest miłować Boga i bliźniego.
+Nadzieja jest ważną chrześcijańską cnotą.
+Kościół regularnie celebruje sakramenty.
+Pan jest moim pasterzem, niczego mi nie braknie.
+Sztuka i muzyka chrześcijańska wyrażają wiarę.
+Biblia oferuje mądrość na codzienne życie.
+Kościół wspiera rodziny i wspólnoty.
+Biblia zawiera opowieści o wierze i odwadze.
+Bóg jest Stwórcą wszystkiego.
+Kościół wierzy w życie po śmierci.
+Tradycja chrześcijańska obejmuje modlitwę, post i jałmużnę.
+Biblia inspiruje ludzi na całym świecie.
+Jezus nazywany jest Barankiem Bożym.
+Duch Święty inspiruje wierzących do czynienia dobra.
+Etyka chrześcijańska opiera się na naukach Jezusa.
+Kościół naucza o godności każdej osoby.
+Słowo Boże rozjaśnia naszą drogę.
+Biblia zachęca do przebaczenia i cierpliwości.
+Błogosławieństwa opisują błogosławieństwa Jezusa.
+Nabożeństwo chrześcijańskie obejmuje śpiew i modlitwę.
+Biblia opowiada historię zbawienia.
+Jezus wezwał swoich uczniów, by za Nim szli.
+Kościół zbudowany jest na fundamencie apostołów.
+Obecność Boża przynosi pociechę i nadzieję.
+Wiara wzrasta przez modlitwę i naukę.
+Kościół pamięta o świętych i męczennikach.
+Jezus uzdrawiał chorych i pocieszał cierpiących.
+Biblia zawiera wiele przysłów i przypowieści.
+Duch Święty jednoczy wierzących w jedno ciało.
+Kościół jest powołany do głoszenia Ewangelii.
+Nadzieja chrześcijańska opiera się na zmartwychwstaniu.
+Biblia naucza, że wszyscy są stworzeni na obraz Boga.
+Bóg zaprasza każdego do relacji z Nim.
+Kościół naucza o sumieniu i wyborach moralnych.
+Modlitwa chrześcijańska może być mówiona lub cicha.
+Biblia zachęca do szukania mądrości.
+Wiara jest wystawiana na próbę w trudnych chwilach.
+Kościół modli się za chorych i cierpiących.
+Biblia naucza o znaczeniu pokory.
+Miłosierdzie Boże jest nieskończone.
+Kościół służy społeczności na wiele sposobów.
+Chrześcijaństwo ceni pokój i sprawiedliwość.
+Biblia opowiada o przymierzu Boga z Abrahamem.
+Kościół obchodzi Pięćdziesiątnicę pięćdziesiąt dni po Wielkanocy.
+Jezus nazywany jest Dobrym Pasterzem.
+Kościół naucza o sensie cierpienia.
+Wierzący są powołani do dawania świadectwa wierze.
+Biblia jest źródłem duchowego pokarmu.
+Kościół czci męczenników wiary.
+Boże przebaczenie przynosi nowe życie.
+Kościół wierzy w moc modlitwy.
+Chrześcijaństwo uczy szacunku do każdego człowieka.
+Biblia mówi, by miłować nieprzyjaciół.
+Tradycja chrześcijańska ceni współczucie.
+Duch Święty daje mądrość i zrozumienie.
+Kościół naucza o życiu i śmierci.
+Nadzieja chrześcijańska przezwycięża lęk.
+Biblia zawiera opowieści o Bożej wierności.
+Jezus jest Zbawicielem świata.
+Kościół jest miejscem nauki i modlitwy.
+Wiara czyni wszystko możliwym.
+Biblia zawiera wiele psalmów pochwalnych.
+Nabożeństwa kościelne obejmują czytania z Biblii.
+Jezus przyjmował dzieci i błogosławił je.
+Kościół naucza o wartości służby.
+Bóg słucha każdej modlitwy.
+Biblia ukazuje Boży plan zbawienia.
+Chrześcijaństwo wierzy w zmartwychwstanie zmarłych.
+Kościół celebruje Wieczerzę Pańską.
+Jezus obiecał Ducha Świętego swoim uczniom.
+Kościół naucza o końcu czasów.
+Biblia zawiera opowieść o Arce Noego.
+Wiara przekazywana jest z pokolenia na pokolenie.
+Kościół oferuje wskazówki dotyczące życia moralnego.
+Chrześcijaństwo ceni uczciwość i prawość.
+Biblia zachęca do hojności.
+Miłość Boża nigdy nie zawodzi.
+Kościół wspiera edukację i naukę.
+Jezus nauczał o miłości do bliźniego.
+Biblia opowiada o życiu króla Dawida.
+Kościół naucza o znaczeniu wspólnoty.
+Chrześcijaństwo miało wpływ na sztukę i kulturę.
+Biblia zawiera proroctwa i mądrość.
+Królestwo Boże jest otwarte dla wszystkich wierzących.
+Kościół obchodzi uroczystości ku czci świętych.
+Duch Święty uzdalnia wierzących do służby.
+Biblia ostrzega przed bałwochwalstwem.
+Chrześcijaństwo naucza o przebaczeniu grzechów.
+Kościół wzywa do jedności wszystkich chrześcijan.
+Jezus jest Księciem Pokoju.
+Biblia jest lampą dla naszych stóp.
+Tradycja chrześcijańska zachowuje starożytne modlitwy.
+Kościół naucza o łasce i zbawieniu.
+Bóg jest wierny swoim obietnicom.
+Biblia zawiera opowieść o Wyjściu z Egiptu.
+Nabożeństwo chrześcijańskie wychwala wielkość Boga.
+Kościół daje pocieszenie w czasie żałoby.
+Duch Święty umacnia naszą wiarę.
+Biblia wzywa nas do troski o stworzenie.
+Chrześcijaństwo naucza o sądzie ostatecznym.
+Kościół zachęca do rozwoju duchowego.
+Jezus przebaczył tym, którzy go ukrzyżowali.
+Biblia opowiada o historii Hioba.
+Słowo Boże przynosi pokój w trudnych czasach.
+Kościół naucza o świętości życia.
+Chrześcijaństwo świętuje nowe początki.
+Biblia uczy, by szukać sprawiedliwości i miłować miłosierdzie.
+Miłość Boża objawia się w Jezusie.
+Kościół gromadzi się na modlitwie i wspólnocie.
+Wiara może przezwyciężyć każdą przeszkodę.
+Biblia zawiera opowieści o prorokach i królach.
+Jezus powiedział: "Ja jestem drogą, prawdą i życiem".
+Kościół chrzci nowych wierzących.
+Chrześcijaństwo zaprasza wszystkich do naśladowania Chrystusa.
+Biblia wzywa nas do życia w sprawiedliwości.
+Bóg jest obecny w każdej chwili.
+Kościół naucza o znaczeniu przebaczenia.
+Duch Święty inspiruje do dobrych uczynków.
+Biblia uczy, by modlić się nieustannie.
+Chrześcijaństwo świętuje narodziny i zmartwychwstanie Jezusa.
+Kościół jest miejscem otwartym dla wszystkich ludzi.
+Wiara przynosi radość sercu.
+Biblia zawiera opowieść o Dobrym Samarytaninie.
+Miłość Boża zmienia życie.
+Kościół zachęca do wdzięczności i dziękczynienia.
+Chrześcijaństwo głosi Królestwo Boże.
+Biblia naucza o wierze, nadziei i miłości.
+Jezus wzywa swoich uczniów do służby innym.
+Kościół niesie światu nadzieję.
+Obietnice Boże są wieczne.
+Biblia jest fundamentem wiary chrześcijańskiej.
+Chrześcijaństwo świętuje przyjście Mesjasza.
+Kościół naucza o darach Ducha Świętego.
+Jezus jest światłością świata.
+Wiara umacnia się we wspólnocie.
+Biblia zachęca nas do szukania Boga.
+Kościół modli się o pokój między narodami.
+Tradycja chrześcijańska ceni ciszę i kontemplację.
+Duch Święty pociesza zasmuconych.
+Biblia opowiada o Bożym wyzwoleniu.
+Miłość Boża trwa na wieki.
+Kościół głosi Ewangelię wszystkim ludziom.
+Chrześcijaństwo uczy o znaczeniu modlitwy.
+Biblia uczy nas troski o ubogich.
+Jezus oferuje przebaczenie wszystkim, którzy się nawracają.
+Kościół jest powołany być światłem dla świata.
+Wiara przynosi uzdrowienie i nadzieję.
+Biblia zachęca do wierności we wszystkim.
+Chrześcijaństwo potwierdza wartość każdego życia.
+Łaska Boża jest wystarczająca dla wszystkich.
+Kościół celebruje dary Ducha Świętego.
+Jezus uczy nas miłości bez granic.
+Biblia inspiruje do dobrych uczynków i miłosierdzia.
+Chrześcijaństwo jest drogą wiary.
+Kościół przyjmuje wszystkich szukających Boga.
+Bóg zaprasza nas, byśmy Mu ufali.
+Biblia naucza o sile wiary.
+Chrześcijaństwo ma bogatą historię świętych i uczonych.
+Kościół ceni prawdę i sprawiedliwość.
+Modlitwa przybliża nas do Boga.
+Biblia zawiera opowieści o odkupieniu i nadziei.
+Obecność Boża daje siłę i odwagę.
+Kościół opowiada się za sprawiedliwością i współczuciem.
+Jezus jest naszym nauczycielem i przyjacielem.
+Chrześcijaństwo wzywa nas do życia w uczciwości.
+Biblia uczy nas przebaczać tak, jak nam przebaczono.
+Miłość Boża jest fundamentem naszego życia.
+Kościół świętuje dar życia.
+Wiara pomaga nam stawiać czoła wyzwaniom z odwagą.
+Biblia jest opowieścią o Bożej miłości do ludzi.
+Jedność chrześcijańska jest znakiem Królestwa Bożego.

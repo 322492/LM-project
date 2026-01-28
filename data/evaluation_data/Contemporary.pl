7c95da547b3b8d24fecd3864ba0e2946e3c4858b
Jakub Kopystianski <jakub.kopystianski@infor.com>
2026-01-28 08:43:05 +0100

    Added 733 contemporry sentences in polish and english

---
+++ data/evaluation_data/Contemporary.pl
@@ -0,0 +1,733 @@
+Wystarczy, osiołku. Wystarczy.
+Rób albo nie rób. Nie ma próbowania.
+Jestem Groot.
+Z wielką mocą wiąże się wielka odpowiedzialność.
+Ku nieskończoności i dalej!
+Ciasto to kłamstwo.
+Nadchodzi zima.
+Nie przejdziesz!
+Zgłaszam się na ochotnika!
+Patrzę na ciebie, dzieciaku.
+Życie jest jak pudełko czekoladek.
+To jest droga.
+Uroczyście przysięgam, że knuję coś niedobrego.
+Expecto Patronum!
+Niech Moc będzie z tobą.
+Niektórzy są warci stopnienia.
+Hakuna Matata!
+Po prostu płyń dalej.
+Odpuść to, odpuść to!
+Jestem królem świata!
+Houston, mamy problem.
+Przywitaj się z moim małym przyjacielem!
+Wrócę.
+Jeśli go zbudujesz, on przyjdzie.
+Widzę umarłych ludzi.
+Elementarne, drogi Watsonie.
+Nie możesz znieść prawdy!
+Dlaczego tak poważnie?
+Hasta la vista, baby.
+Zachowaj spokój i idź dalej.
+Żyje się tylko raz.
+Świat stoi przed tobą otworem.
+Nie wszyscy, którzy błądzą, są zagubieni.
+Wszystko, czego potrzebujesz, to miłość.
+Tańcz, jakby nikt nie patrzył.
+Nie trafiasz żadnego rzutu, którego nie wykonasz.
+Żyj, śmiej się, kochaj.
+Idę w promieniach słońca.
+Nie przestawaj wierzyć.
+Chcę się uwolnić.
+Przetrwam.
+To piękny dzień, nie pozwól mu przeminąć.
+Po prostu to zrób.
+Czuję, że dzisiejsza noc będzie dobra.
+Cześć, to ja.
+Jestem na szczycie świata.
+To moja piosenka walki.
+Świeć jasno jak diament.
+Jestem zbyt seksowny na tę koszulę.
+Ups!... Znów to zrobiłem.
+Jestem nie do zatrzymania.
+Wciąż stoję.
+Nie zawsze dostajesz to, czego chcesz.
+Zacznijmy imprezę.
+Nie jestem bohaterem. Nigdy nie byłem. Nigdy nie będę.
+Potrzebuję bohatera.
+Nie martw się, bądź szczęśliwy.
+Wszystko jest super!
+Zmiażdżymy was!
+Jestem twardy, przetrwam.
+Obudź mnie zanim wyjdziesz.
+Mam oko tygrysa.
+Słodkie sny są z tego zrobione.
+Wpadłem tu jak kula demolkowa.
+Czuję się dobrze.
+To już ostatnie odliczanie.
+Nie zapomnij o mnie.
+Jesteśmy rodziną!
+Obracasz mnie w koło.
+Jesteś po prostu najlepsza.
+Moje serce będzie trwać.
+Jestem dziewczyną Barbie, w świecie Barbie.
+Baby shark, du du du du du du.
+Nigdy cię nie opuszczę.
+Mam ochotę tańczyć.
+Człowiek w lustrze.
+Masz we mnie przyjaciela.
+Chcę to właśnie.
+Wierzę, że potrafię latać.
+Przynoszę seksapil z powrotem.
+Zawsze będę cię kochać.
+Imprezuj jakby był rok 1999.
+Padają mężczyźni.
+Nie zatrzymuj mnie teraz.
+Każdy chce rządzić światem.
+Viva la vida.
+Licząc gwiazdy.
+Jestem twój.
+Pokerowa twarz.
+Tocząc się w głębi.
+Nie mogę przestać czuć się dobrze!
+Szczęśliwi razem.
+Jestem niebieski da ba dee da ba daa.
+Nie zawiedź mnie.
+Znaleźliśmy miłość.
+Jestem wierzącym.
+Alleluja.
+Kształt ciebie.
+Zadzwoń do mnie może.
+Do zobaczenia znów.
+Cały ja.
+Weź mnie.
+Płytko.
+Wierzący.
+Grzmot.
+Senorita.
+Wspomnienia.
+Zły chłopak.
+Stara miejska droga.
+Słonecznik.
+Despacito.
+Oślepiające światła.
+Tańcząca małpa.
+Ktoś, kogo kochałeś.
+Savage love.
+Arbuzowy cukier.
+Levitating.
+Zostań.
+Brzoskwinie.
+Fale upału.
+Astronauta w oceanie.
+Gęsia skórka.
+Nastrój.
+Montero.
+Masło.
+Zezwolenie na taniec.
+Dynamit.
+Życie toczy się dalej.
+Chłopak z miłością.
+Fałszywa miłość.
+DNA.
+Ogień.
+Sklep z magią.
+Wiosenny dzień.
+Czarny łabędź.
+Niebieski i szary.
+Idol.
+Mikrokosmos.
+Euforia.
+Szczęśliwy traf.
+Epifania.
+Filtr.
+Mikrokosmos.
+Dom.
+Zrób to dobrze.
+Jutro.
+Nigdy nie myśl.
+Biegnij.
+Ratuj mnie.
+Młody na zawsze.
+Nie dziś.
+Odlot.
+Potrzebuję cię.
+Krew, pot i łzy.
+Skrzydła.
+Początek.
+Stigma.
+Kłamstwo.
+Obudź się.
+Zagubiony.
+Pierwsza miłość.
+Odbicie.
+MAMA.
+Kochaj siebie.
+Odpowiedź.
+Ona.
+Łza.
+Mapa duszy.
+Persona.
+Cień.
+Ego.
+Na.
+Przyjaciele.
+Jamais vu.
+Dołeczek.
+Najlepsze z mnie.
+Go go.
+Anpanman.
+Niewypowiedziana prawda.
+Morze.
+Odpuść.
+Kryształowy śnieg.
+Nie zostawiaj mnie.
+Więc co.
+Magia.
+Światło.
+Samolot pt.2.
+134340.
+Raj.
+Labirynt miłości.
+Wyspa magii.
+Bicie serca.
+Światła.
+Bądź złoty.
+Twoje oczy mówią.
+Film out.
+Dla ciebie.
+Czuję się dobrze.
+Obietnica.
+Wieczny deszcz.
+Dzisiejsza noc.
+Zimowy niedźwiedź.
+Księżyc.
+Słodka noc.
+Wewnętrzne dziecko.
+Kwiat śniegu.
+Niebieska strona.
+Sceneria.
+Wciąż z tobą.
+Miłość świąteczna.
+Mój wszechświat.
+Samotny.
+Polaroidowa miłość.
+Uwaga.
+Hype boy.
+Ditto.
+OMG.
+Kupidyn.
+Super nieśmiały.
+ETA.
+Nowe Jeansy.
+Ciasteczko.
+Zero.
+Cool with you.
+Wstań.
+Ból.
+Fantastyczny dzieciak.
+Bang bang bang.
+Ostatni taniec.
+Dobry chłopak.
+Przegrany.
+Jeśli ty.
+Nie zakochujmy się.
+FXXK IT.
+Trzeźwy.
+Bae Bae.
+Lubimy imprezować.
+Krzywy.
+Tęsknię za tobą.
+Scenariusz miłosny.
+Zabijasz mnie.
+Głupi głupi.
+Jak ci się podoba?
+Lody.
+Solo.
+DDU-DU DDU-DU.
+BOOMBAYAH.
+Gwizdek.
+Zabawa z ogniem.
+Zostań.
+Jakby to była ostatnia.
+Wiecznie młodzi.
+Naprawdę.
+Do zobaczenia później.
+Rozruszaj to.
+Nie wiem co robić.
+Oby nie.
+Dość dziki.
+Dziewczyny z miłosnym bólem.
+Nigdy nie wiesz.
+Założę się, że chcesz.
+Różowa trucizna.
+Zamknij to.
+Typ dziewczyny.
+Gotowa na miłość.
+Tally.
+Tak tak tak.
+Trudno kochać.
+Najszczęśliwsza dziewczyna.
+Ciąg dalszy nastąpi...
+Dawno, dawno temu w dalekiej krainie.
+Żyli długo i szczęśliwie.
+Koniec.
+Lustereczko, powiedz przecie, kto jest najpiękniejszy na świecie?
+Fee-fi-fo-fum.
+Roszpunko, Roszpunko, opuść swoje włosy!
+Spóźniam się, spóźniam się, na bardzo ważne spotkanie!
+Skrócić im głowy!
+Zegar wybił północ.
+Bibbidi-bobbidi-boo!
+Nadmucham i rozwalę twój domek!
+Prosiaczku, prosiaczku, wpuść mnie do środka!
+Kto się boi wielkiego złego wilka?
+Żeby cię zjeść, kochanie!
+Śnieżka ugryzła jabłko i zasnęła.
+Kopciuszek zostawiła swój pantofelek.
+Jacek wspiął się po fasoli.
+Brzydkie kaczątko stało się łabędziem.
+Złotowłosa znalazła odpowiednią owsiankę.
+Jaś i Małgosia szli za okruszkami.
+Księżniczka pocałowała żabę.
+Śpiąca Królewna ukłuła się i spała sto lat.
+Ciasteczkowy chłopiec biegł, jak mógł najszybciej.
+Piotruś Pan nie chciał dorosnąć.
+Dzwoneczek posypała magicznym pyłem.
+Wendy wierzyła w magię.
+Kapitan Hak przegrał z krokodylem.
+"Mogę latać!" zawołał Piotruś.
+Nibylandia to miejsce marzeń.
+Pył wróżek pozwala latać.
+Nie ma to jak w domu.
+Podążaj żółtą brukowaną drogą.
+Dopadnę cię, moja droga, i twojego małego psa też!
+Gdzieś za tęczą.
+Lwy, tygrysy i niedźwiedzie, ojej!
+Kto śmie zakłócać mój sen?
+Niech żyje król!
+Hakuna Matata!
+Krąg życia.
+Odpuść to!
+W nieznane.
+Chcesz ulepić bałwana?
+Widzę światło.
+Całkiem nowy świat.
+Pod wodą.
+Część twojego świata.
+Pocałuj ją.
+Czy czujesz dziś miłość?
+Masz we mnie przyjaciela.
+Nie ma za co!
+Pokonaj dystans.
+Od zera do bohatera.
+Nie powiem, że jestem zakochana.
+Będziesz w moim sercu.
+Obcy tacy jak ja.
+Spróbuj wszystkiego.
+Nie w Nottingham.
+Kolory wiatru.
+Odbicie.
+Jesteś tym, kogo chcę.
+Letnie noce.
+Błyskawica Greased.
+Bez nadziei oddana tobie.
+Pozostać przy życiu.
+Królowa tańca.
+Waterloo.
+Mamma Mia!
+Super Trouper.
+Gimme! Gimme! Gimme!
+Nie zatrzymuj mnie teraz.
+Bohemian Rhapsody.
+Jesteśmy mistrzami.
+Ktoś do kochania.
+Radio Gaga.
+Szalone, małe coś zwane miłością.
+Chcę się uwolnić.
+Kolejny gryzie piach.
+Widzę mały cień człowieka.
+Scaramouche, Scaramouche, zatańczysz Fandango?
+Błyskawice i grzmoty, bardzo, bardzo mnie przerażają.
+Galileo, Figaro, Magnifico!
+Przetrwam.
+Jestem taka podekscytowana!
+Mam moc!
+Powinnaś zobaczyć mnie w koronie.
+Zły chłopak.
+Urocza.
+Oczy oceanu.
+Wszystko, czego chciałam.
+Nie ma czasu na śmierć.
+Zagubiona przyczyna.
+Dlatego jestem.
+Szczęśliwsza niż kiedykolwiek.
+Bellyache.
+Kiedy impreza się kończy.
+Chciałbym, żebyś był gejem.
+Moja przyszłość.
+Twoja moc.
+NDA.
+Lubię cię.
+Kręgi.
+Słonecznik.
+Wow.
+Pożegnania.
+Rockstar.
+Gratulacje.
+Lepiej teraz.
+Psychopata.
+Weź co chcesz.
+Hollywood krwawi.
+Saint-Tropez.
+Oślepiające światła.
+W twoich oczach.
+Ocal swoje łzy.
+Bez serca.
+Po godzinach.
+Gwiezdny chłopak.
+Zawołaj mnie po imieniu.
+Czuję, że nadchodzi.
+Wzgórza.
+Nie czuję twarzy.
+Sekrety.
+Módl się za mnie.
+Zasłużyłem.
+Często.
+Kochaj mnie mocniej.
+Złe gry.
+Wysoko z tym.
+Dom balonów.
+Poranek.
+Schodzę.
+W nocy.
+Zaznajomiony.
+Bezwstydny.
+Za późno.
+Ucieczka z LA.
+Powtórz za mną.
+Dopóki nie wykrwawię się.
+Ostatnia kołysanka.
+Poza czasem.
+Mniej niż zero.
+Benzyna.
+Poświęcenie.
+Nie wszystko złoto, co się świeci.
+Każda chmura ma srebrną otoczkę.
+Grosz za twoje myśli.
+Czyn mówi więcej niż słowa.
+Ptaki tego samego gatunku trzymają się razem.
+Nie licz kurczaków przed wykluciem.
+Kto rano wstaje, temu Pan Bóg daje.
+Patelnia pilnowana nigdy nie zawrze.
+Najlepsze rzeczy w życiu są za darmo.
+Lepiej późno niż wcale.
+Jeden obraz wart tysiąc słów.
+Piękno jest w oku patrzącego.
+Gdy jesteś w Rzymie, rób jak Rzymianie.
+Trawa jest zawsze bardziej zielona po drugiej stronie.
+Rzymu nie zbudowano w jeden dzień.
+Głośne koło dostaje smar.
+Co dwie głowy, to nie jedna.
+Nie oceniaj książki po okładce.
+Pióro jest potężniejsze niż miecz.
+Nie ma lepszego czasu niż teraz.
+Jabłko nie pada daleko od jabłoni.
+Nie wkładaj wszystkich jaj do jednego koszyka.
+Kuj żelazo póki gorące.
+Zszywka w porę oszczędza dziewięć.
+Czyn mówi więcej niż słowa.
+Dowód jest w budyniu.
+Ciekawość zabiła kota.
+Nieobecność sprawia, że serce pragnie.
+Nie budź śpiącego psa.
+Nie gryź ręki, która cię karmi.
+Nie rób z igły wideł.
+Toczący się kamień nie obrasta mchem.
+Ten, kto się śmieje ostatni, śmieje się najlepiej.
+Jeśli coś działa, nie poprawiaj tego.
+Do tanga trzeba dwojga.
+Nie oceniaj książki po okładce.
+Praktyka czyni mistrza.
+Czego oczy nie widzą, tego sercu nie żal.
+Krew nie woda.
+Piłka jest po twojej stronie.
+Nie płacz nad rozlanym mlekiem.
+Nie odkładaj na jutro tego, co możesz zrobić dziś.
+Wszystko dobre, co się dobrze kończy.
+Prawdziwy przyjaciel poznaje się w biedzie.
+Przedstawienie musi trwać.
+Czas leci, gdy dobrze się bawisz.
+Dom jest tam, gdzie serce.
+Łatwo przyszło, łatwo poszło.
+Nie wspominaj przeszłości.
+Niewiedza to błogosławieństwo.
+Czyn mówi więcej niż słowa.
+Każdy pies ma swój dzień.
+Lepszy wróbel w garści niż gołąb na dachu.
+Nie możesz mieć ciastka i zjeść ciastka.
+To nie jest fizyka rakietowa.
+Stąpasz po cienkim lodzie.
+Powodzenia!
+Nie oceniaj książki po okładce.
+Raz ukąszony, dwa razy ostrożny.
+Nie owijaj w bawełnę.
+Możesz doprowadzić konia do wodopoju, ale nie zmusisz go do picia.
+Cisza przed burzą.
+Daj komuś korzyść z wątpliwości.
+Nie wylewaj dziecka z kąpielą.
+Diabeł tkwi w szczegółach.
+Nie zaglądaj darowanemu koniowi w zęby.
+Nie licz kurczaków przed wykluciem.
+Oszczędzaj grosz, zarobisz grosz.
+Uczciwość popłaca.
+To napisane na ścianie.
+Łańcuch jest tak silny, jak jego najsłabsze ogniwo.
+Nie nauczysz starego psa nowych sztuczek.
+Nie oceniaj książki po okładce.
+Nie wkładaj wszystkich jaj do jednego koszyka.
+Gdy pod górę, twardzi idą dalej.
+Rób siano, kiedy słońce świeci.
+Nie oceniaj książki po okładce.
+Głośne koło dostaje smar.
+Kto rano wstaje, temu Pan Bóg daje.
+Nie wszystko złoto, co się świeci.
+Patelnia pilnowana nigdy nie zawrze.
+Czyn mówi więcej niż słowa.
+Rzymu nie zbudowano w jeden dzień.
+Najlepsze dopiero przed tobą.
+Żyj i pozwól żyć innym.
+Zbierasz to, co posiejesz.
+Jeśli chcesz tęczy, musisz wytrzymać deszcz.
+Nie przejmuj się drobiazgami.
+Głowa do góry.
+Nie możesz wygrać wszystkiego.
+Dobre rzeczy przychodzą do cierpliwych.
+Człowiek się uczy całe życie.
+Bez bólu nie ma zysku.
+Co się dzieje, wraca.
+Nie oceniaj książki po okładce.
+Lepiej dmuchać na zimne.
+Czas leczy rany.
+Gdzie jest wola, tam jest sposób.
+Nigdy nie mów nigdy.
+Życie jest krótkie.
+Chwytaj dzień!
+Podążaj za sercem.
+Marz wielkie marzenia!
+Pracuj ciężko, baw się dobrze.
+Uwierz w siebie.
+Bądź sobą; wszyscy inni już są zajęci.
+Jeśli potrafisz to wymarzyć, potrafisz to zrobić.
+Żyje się tylko raz.
+Żyj, śmiej się, kochaj.
+Miłość wszystko zwycięża.
+Najlepsze dopiero przed tobą.
+Idź dalej.
+Wszystko dzieje się z jakiegoś powodu.
+Niebo jest granicą.
+Nigdy się nie poddawaj.
+Nie martw się, bądź szczęśliwy.
+Uśmiech to darmowa terapia.
+Rób to, co kochasz.
+Bądź zmianą, którą chcesz widzieć na świecie.
+Każda chwila się liczy.
+Życie jest tym, co z niego zrobisz.
+Jesteś wystarczający.
+Skocz na głęboką wodę.
+Znajdź radość w podróży.
+Bądź pozytywny.
+Skup się na dobrym.
+Wybierz szczęście.
+Możesz wszystko.
+Twój nastrój przyciąga twoją ekipę.
+Zbieraj chwile, nie rzeczy.
+Żyj najlepiej, jak potrafisz.
+Przyszłość zaczyna się teraz.
+Spełnij to.
+Życzliwość nigdy nie wychodzi z mody.
+Nigdy nie jesteś za stary, by wyznaczyć nowy cel.
+Tylko dobre wibracje.
+Postęp, nie perfekcja.
+Pozwól swojemu światłu świecić.
+Małe kroki codziennie.
+Szczęście ci pasuje.
+Nie oglądaj się za siebie, idziesz w inną stronę.
+Świętuj każdy sukces.
+Ciesz się drobiazgami.
+Jesteś silniejszy, niż myślisz.
+Wdzięczność zmienia wszystko.
+Życie to podróż, nie cel.
+Twoja historia jeszcze się nie skończyła.
+Twoim jedynym ograniczeniem jesteś ty sam.
+Rób to z pasją.
+Niech dzisiaj będzie niesamowity dzień.
+Marzenia nie działają, jeśli ty nie pracujesz.
+Niech marzenia będą twoimi skrzydłami.
+Jesteś autorem swojej historii.
+Nie czekaj na okazję. Stwórz ją.
+Ryzykuj, popełniaj błędy, ubrudź się!
+Wkładaj serce we wszystko, co robisz.
+Uśmiech to najpiękniejszy dodatek.
+Życie jest trudne, ale ty też.
+Zacznij każdy dzień z wdzięcznym sercem.
+Kochaj siebie.
+Podążaj za marzeniami.
+Nigdy nie przestawaj się uczyć.
+Dasz radę!
+Nie ma rzeczy niemożliwych.
+Pozostań skromny.
+Życzliwość nic nie kosztuje.
+Bądź nieustraszony w dążeniu do tego, co rozpala twoją duszę.
+Nie pozwól nikomu zgasić twojego blasku.
+Nie tylko istnień, żyj.
+Sukces nie jest ostateczny, porażka nie jest śmiertelna.
+Twój czas jest teraz.
+Śmiej się ile możesz.
+Pokój zaczyna się od uśmiechu.
+Zaufaj procesowi.
+Nie pozwól, by wczoraj zabrało za dużo dzisiaj.
+Wydaje się niemożliwe, dopóki nie zostanie zrobione.
+Gwiazdy nie świecą bez ciemności.
+Bądź głosem, nie echem.
+Jeśli nie teraz, to kiedy?
+Żyj pełnią życia.
+Twórz własną magię.
+Zrób dziś coś, za co jutro sobie podziękujesz.
+Wielkie rzeczy nie pochodzą ze strefy komfortu.
+Życie zaczyna się na końcu strefy komfortu.
+Najlepszy widok pojawia się po najtrudniejszej wspinaczce.
+Nigdy nie jest za późno, by zacząć od nowa.
+Wszystko, czego potrzebujesz, to miłość.
+Bądź słońcem.
+Ryzykuj.
+Nie pozwól, by marzenia były tylko marzeniami.
+Bądź obecny.
+Życie jest lepsze, gdy się śmiejesz.
+Zbieraj piękne chwile.
+Jesteś niesamowity taki, jaki jesteś.
+Jesteś zdolny do niesamowitych rzeczy.
+Bądź głodny. Bądź szalony.
+Piękno tkwi w prostocie.
+Uwierz, że możesz, a już jesteś w połowie drogi.
+Idź dalej.
+Zawsze wybieraj miłość.
+Nie pozwól, by strach cię powstrzymał.
+Najlepsza droga prowadzi zawsze przez trudności.
+Żyj prosto.
+To, czego szukasz, szuka ciebie.
+Dobre rzeczy wymagają czasu.
+Rób to, co sprawia, że twoja dusza jest szczęśliwa.
+Nie zadowalaj się byle czym.
+Każdy dzień to nowy początek.
+Robisz różnicę.
+Pamiętaj, dlaczego zacząłeś.
+Twój nastrój decyduje o kierunku.
+Uczyń swoje życie dziełem sztuki.
+Pozwól sercu prowadzić.
+Bądź ciekawy.
+Świeć dalej.
+W porządku jest nie być w porządku.
+Bądź dla siebie miły.
+Nie każdy dzień jest dobry, ale w każdym dniu jest coś dobrego.
+Bądź dla siebie delikatny.
+Błędy są dowodem, że próbujesz.
+Upadnij siedem razy, wstań osiem.
+Nie porównuj swojego początku do czyjegoś końca.
+Zachowaj dzikość serca.
+Dotrzymuj obietnic.
+Podróż jest nagrodą.
+Bądź odważny.
+Niech twoja wiara będzie większa niż strach.
+Nie jesteś sam.
+Wierz w cuda.
+Niech twoja życzliwość zaleje świat.
+Twórz wspomnienia.
+Bądź swoim rodzajem piękna.
+Jesteś wolny być sobą.
+Jesteś silniejszy niż wczoraj.
+Żyj chwilą.
+Twoje marzenia są ważne.
+Każdy zachód słońca to szansa na reset.
+Niech nadzieja będzie kotwicą duszy.
+Jesteś powodem czyjegoś uśmiechu dziś.
+Rób małe rzeczy z wielką miłością.
+Najlepsze przygotowanie na jutro to dać wszystko dziś.
+Kieruj się miłością.
+Znajdź swój ogień.
+Sukces to podróż, nie cel.
+Wybierz mniej uczęszczaną drogę.
+Przygoda czeka.
+Bądź tęczą w czyjejś chmurze.
+Zostaw swój ślad.
+Żyj kolorowo.
+Marzenia są ziarnami zmiany.
+Rozwijaj się przez to, przez co przechodzisz.
+Piękno zaczyna się, gdy decydujesz się być sobą.
+Pogódź się z przeszłością.
+Twoim jedynym ograniczeniem jest umysł.
+Żyj z zamiarem.
+Zatrzymaj się i pomyśl.
+Świat potrzebuje tego, co masz.
+Masz znaczenie.
+Idź tam, gdzie czujesz się najbardziej żywy.
+Wybierz radość.
+Pozostań wierny sobie.
+Twoja odmienność to twoja siła.
+Pomysły stają się rzeczami.
+Nigdy nie jesteś sam.
+Świeć jasno.
+Jesteś w trakcie tworzenia.
+Bądź obecny dla siebie.
+Nigdy nie trać poczucia zachwytu.
+Bądź światłem.
+Twoja życzliwość ma znaczenie.
+Zaryzykuj nieznane.
+Znajdź czas na odpoczynek.
+Niech twoja odwaga zaraża innych.
+Odważ się mocno.
+Bądź powodem, dla którego ktoś wierzy w dobrych ludzi.
+W wrażliwości jest siła.
+Twoja przyszłość powstaje z tego, co robisz dziś.
+Bądź sobą bez przeprosin.
+Jesteś godny miłości i szacunku.
+Wznieś się ponad burzę.
+Niech twój uśmiech zmienia świat.
+Dawaj bez oczekiwań.
+Bądź marzycielem.
+Nigdy siebie nie lekceważ.
+Słuchaj serca.
+Nie musisz mieć wszystkiego zaplanowanego, by ruszyć naprzód.
+Odpuść to, czego nie możesz kontrolować.
+W nowych początkach jest magia.
+Porażka to tylko kolejny krok do sukcesu.
+Życie jest krótkie, niech będzie słodkie.
+Trzymaj głowę wysoko.
+Żyj bez żalu.
+Bądź wdzięczny za dziś.
+Jesteś błogosławiony.
+Pozwól sobie rozkwitnąć.
+Rozświetl świat.
+Szukaj przygody.
+Świętuj siebie.
+Każde zakończenie jest nowym początkiem.
+Gonisz słońce.
+Jesteś wystarczający, zawsze byłeś.
+Okazuj miłość.
+Podnoś innych na duchu.
+Życzliwość jest zaraźliwa.
+Nigdy nie zapominaj, jak daleko zaszedłeś.
+Ciesz się podróżą.
+Możesz poprosić o pomoc.
+Jesteś potężniejszy niż myślisz.
+Szukaj dobra w każdej sytuacji.
+Znajdź piękno w każdym dniu.
+Bądź dobry.
+Żyj z sercem.
+Pozostań silny.
+Radzi sobie świetnie.
+Przegrywasz tylko, gdy przestajesz próbować.
+Daj sobie szczęście.
+Należysz tu.
+Najlepsze dopiero przed tobą.
+Miłość jest zawsze odpowiedzią.
+Sam tworzysz swoje przeznaczenie.
